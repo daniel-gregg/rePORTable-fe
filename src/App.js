7e21ceb772764ebab125f3c14ac4f68be1d9e4f7
Spaniel Boone <spaniel.boone@gmail.com>
2021-08-09 18:35:33 +0930

    update with hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
